3a8ea094965b6ebb7a3654ad6e2630df55049b46
Ile Eftimov <ileeftimov@gmail.com>
2015-10-19 23:40:31 +0200

    add lots of stuff

--- lib/curtains.ex
+++ lib/curtains.ex
@@ -1,2 +1,20 @@
 defmodule Curtains do
+  import Plug.Conn
+
+  def init(_opts) do
+    Application.get_env(:curtains, :file_path)
+  end
+
+  def call(conn, file) do
+    case File.read(file) do
+      {:ok, binary } ->
+        conn
+        |> put_resp_content_type("text/html")
+        |> send_resp(200, binary)
+      {:error, reason } ->
+        conn
+    end
+  end
 end
+
+
