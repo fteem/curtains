e77d57dc4aa3c3fd35c57c7393d80462dc870f0f
Ile Eftimov <ileeftimov@gmail.com>
2015-10-20 00:40:54 +0200

    halt connection

--- lib/curtains.ex
+++ lib/curtains.ex
@@ -11,6 +11,7 @@ defmodule Curtains do
         conn
         |> put_resp_content_type("text/html")
         |> send_resp(200, binary)
+        |> halt
       {:error, reason } ->
         IO.inspect reason
         conn
